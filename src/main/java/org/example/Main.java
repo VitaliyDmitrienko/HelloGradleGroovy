b059e93d21bd6252a94e1d8e88a3cf7d24d62922
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-07-12 19:03:07 +0300

    1st. (Error: no main manifest attribute, in ... ) Why?

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Gradle!");
+        System.out.println(StringUtils.upperCase("Hello Gradle!!!"));
+
+    }
+}
