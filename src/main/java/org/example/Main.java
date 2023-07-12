a08296f7ce2c77ce99731f06f0abe096139f0c4d
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-07-12 19:03:07 +0300

    2nd. Add manifest to run JAR.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,8 @@ import org.apache.commons.lang3.StringUtils;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Gradle!");
-        System.out.println(StringUtils.upperCase("Hello Gradle!!!"));
+        System.out.println("Hello Gradle! (stdout)");
+        System.out.println(StringUtils.upperCase("Hello Gradle!!! (via commons.lang3.StringUtils)"));
 
     }
 }
